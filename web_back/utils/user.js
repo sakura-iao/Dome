d9a415b38b6ef80d4eb24fdc4003c96e632f6fa6
saku <Sakura@qq.com>
2019-12-28 12:27:50 +0800

    后台工具文件

---
+++ web_back/utils/user.js
@@ -0,0 +1,29 @@
+
+var user = {
+    logout: function () {
+        $.post(baseUrl + '/admin/logout', function (res) {
+            alert(res.msg);
+            if (res.code === 200) {
+                // 退出成功，跳转到登录页面
+                location.href = 'login.html';
+            }
+        });
+    },
+
+    login: function (username, password) {
+        $.post(
+            baseUrl + '/admin/login',
+            { user_name: username, password: password },
+            function (res) {
+                console.log(res);
+                console.log(res.code);
+
+                alert(res.msg);
+                if (res.code == 200) {
+                    location.href = 'index.html';
+                }
+            },
+            'json'
+        )
+    }
+}
